167dd1f1ababa61ae5a6a94cb75686387466b506
John Attai Yinusa <donattai1032@gmail.com>
2023-08-06 16:49:39 +0100

    add axiosInstance

---
+++ src/api/axios.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.token) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("user");
+      window.location.reload(true);
+      return;
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
